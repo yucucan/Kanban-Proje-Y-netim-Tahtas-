2ee04fb624615bd5f89497557e381b239a06ce21
Yusufcan Demirkapı <147403225+yucucan@users.noreply.github.com>
2026-04-26 23:33:28 +0300

    Refactor Supabase client creation method

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,10 @@
-import { createBrowserClient } from '@supabase/ssr'
+import { createClient as supabaseCreateClient } from '@supabase/supabase-js'
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+
+export const supabase = supabaseCreateClient(supabaseUrl, supabaseAnonKey)
 
 export function createClient() {
-  return createBrowserClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-  )
+  return supabase
 }
